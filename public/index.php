48657d8d033ec7d31836a495e80cbcbe4fbff6db
ilumos <ilumos@gmail.com>
2015-06-14 21:13:56 +0100

    Refactoring use of DateTime object

--- public/index.php
+++ public/index.php
@@ -1,20 +1,19 @@
 <?php namespace Zeropingheroes\Lanager\Core\Domain;
 
+use DateTime;
+
 require '../vendor/autoload.php';
 
 $user = new User\User( 'ilumos' );
 
 $shout = new Shout\Shout( $user, 'Hello LAN');
 
-$start = new \DateTime('2015-06-14 14:00:00');
-$end = new \DateTime('2015-06-14 18:00:00');
-
-$event = new Event\Event( 'Pizza Time', $start, $end );
+$event = new Event\Event( 'Pizza Time', new DateTime('2015-06-14 14:00:00'), new DateTime('2015-06-14 18:00:00') );
 
 $guide = new Guide\Guide( 'LAN Party Rules', 'No diving' );
 
 $achievement = new Achievement\Achievement( 'Bonk!', 'Drink 5 caffinated beverages' );
 
-$lan = new Lan\Lan( 'Zero Ping Heroes LAN 13', new \DateTime('2015-06-12 18:00:00'), new \DateTime('2015-06-14 18:00:00') );
+$lan = new Lan\Lan( 'Zero Ping Heroes LAN 13', new DateTime('2015-06-12 18:00:00'), new DateTime('2015-06-14 18:00:00') );
 
 var_dump( $lan );
